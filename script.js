9198ab7a4c9ad20cf8c142fc59606f0e3f3285a4
Jaysquare <jamiuolajide20@gmail.com>
2024-12-05 19:35:48 -0600

    semantic feature and JavaScript modification

--- script.js
+++ script.js
@@ -32,3 +32,86 @@ let displayAnswer = (event)=>{
 
 //Event Listener 
 submitButton.addEventListener('click', displayAnswer);
+
+// INSPIRATIONAL MESSAGE GENERATOR JAVASCRIPT CODES
+// Random Number Function
+let getRandom= num =>{
+    return Math.round(Math.random()*num);
+}
+
+// Array Objects of Messages
+let arrayCollections ={
+    firstMessages: ["The best use of life is LOVE.", 
+            "The best expression of love is TIME.",
+            "The best time to love is NOW."  ],
+
+    secondMessages: ["Every temptation is an opportunity to do good.",
+              "It is easier to stay out of temptation than getting out of it.",
+              "The quicket way to neutralise temptation allure is to turn your attention to something else."],
+     
+    thirdMessages:["Observe your spiritual gift.",
+              "Listen to your heart.",
+              "Use your ability."]
+}
+
+// Global variable of message generated
+let messagesGenerated;
+
+// Random Generate Messages Function
+let generateMessages =()=>{
+ messagesGenerated =[];
+
+
+ for(let message in arrayCollections) {
+    let selectedOption = getRandom(arrayCollections[message].length-1);
+    
+    switch (message){
+        case 'firstMessages':
+         messagesGenerated.push(arrayCollections[message][selectedOption])
+         break;
+     
+        case 'secondMessages':
+         messagesGenerated.push(arrayCollections[message][selectedOption])
+         break;
+     
+        case 'thirdMessages':
+         messagesGenerated.push(arrayCollections[message][selectedOption])
+         break;
+     
+        default:
+         messagesGenerated.push( "There is a purpose behind every situation( problem and fortune)");
+         
+       }
+
+  }
+  return messagesGenerated;
+};
+
+// Generated Messages Format function
+const formatMessage =(messages)=>{
+    let format = messages.join("\n");
+     return format;
+}
+
+// DOM Variables
+let runMessageClick = document.getElementById('run-message');
+let button = document.getElementById('gen-button');
+let messageProject = document.getElementById('msg-project-box');
+    messageProject.hidden = true;
+
+//Events Functions
+let displayProject = ()=>{
+    messageProject.hidden = false;
+    
+}
+
+let displayMessage = ()=>{
+    let msgDisplay=document.getElementById('msg-display');
+    messagesGenerated = generateMessages();
+    msgDisplay.innerHTML = formatMessage(messagesGenerated);       
+}
+// Add Events Listeners
+button.addEventListener('click', displayMessage);
+runMessageClick.addEventListener('click', displayProject);
+
+
