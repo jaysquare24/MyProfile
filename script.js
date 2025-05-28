1561d2966547e1bb45e394ac28fcbc03cfabf39c
Jaysquare <jamiuolajide20@gmail.com>
2025-05-27 18:01:54 -0600

    modify project section

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*-------- MENU TOGGLE-----------------*/
 
 /* DOM Elements*/
+const body = document.querySelector('.body');
 const menuIcon = document.getElementById('menu-icon');
 const menuButton = document.getElementById('menu-button');
 const nav = document.getElementById('hidden');
@@ -13,9 +14,13 @@ function toggleMenu() {
 
   if (menuIcon.src.endsWith("icons8-menu-24.png")) {
       menuIcon.src = "resources/icons8-cancel-24.png";
+      
   } else {
       menuIcon.src = "resources/icons8-menu-24.png";
+     
   }
+  
+
 }
 
 /* Event Listeners*/
