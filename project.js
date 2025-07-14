e99d2a11670cc20b031b02c617b7bd7888bf13ed
Jaysquare <jamiuolajide20@gmail.com>
2025-07-14 16:05:11 -0600

    add ecommerce wep app to projects

--- project.js
+++ project.js
@@ -1,19 +1,19 @@
 /*PROJECT PAGE FILTER*/
 
-document.addEventListener('DOMContentLoaded', function() {
-    const projectSections = document.querySelectorAll('.project-section');
-    // Get the hash from the URL (e.g., #expense-tracker)
-    const hash = window.location.hash.replace('#', '');
+document.addEventListener('DOMContentLoaded', function () {
+  const projectSections = document.querySelectorAll('.project-section');
+  const hash = window.location.hash.replace('#', '');
 
-    if (hash) {
+  if (hash) {
     projectSections.forEach(section => {
-    section.style.display = section.id === hash? 'block' : 'none';
-        
+      if (section.id === hash) {
+        section.classList.remove('hidden');
+      } else {
+        section.classList.add('hidden');
+      }
     });
-    } else {
-    // If no hash, show all or just the first section
-    projectSections.forEach((section, i) => {
-        section.style.display = i === 0 ? 'block' : 'none';
-    });
-    }
+  }
 });
+
+
+
